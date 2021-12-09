89158716a18cb8fef5fb6f4cbc47b9a982d05380
nongdanxomdua <thanhthientran1511@gmail.com>
2021-12-09 18:30:44 +0700

    edit format

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import anhDaiDien from './images/anh_dai_dien.jpg';
-import eLaptop from './images/logo_E-Laptop_complete.png';
-import Navigation from './component/Navigation';
-import Header from './component/Header';
-import Dashboard from './component/Dashboard';
+import { Routes, Route } from "react-router-dom";
+
+// import anhDaiDien from './images/anh_dai_dien.jpg';
+// import eLaptop from './images/logo_E-Laptop_complete.png';
+// import Navigation from './component/Navigation';
+// import Header from './component/Header';
+// import Dashboard from './component/Dashboard';
+
+import Dashboard from './views/dashboard'
 
 function App() {
   return (
-    <div className="App">
-      <Header logo={eLaptop}></Header>
-       <Navigation proImg={anhDaiDien} userName="Huy"></Navigation>
-       <div className="page_content">
-        <Dashboard img={eLaptop}></Dashboard>
-       </div>
+    // <div className="App">
+    //   <Header logo={eLaptop}></Header>
+    //    <Navigation proImg={anhDaiDien} userName="Huy"></Navigation>
+    //    <div className="page_content">
+    //     <Dashboard img={eLaptop}></Dashboard>
+    //    </div>
+    // </div>
+
+    <div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </div>
+
   );
 }
 export default App;
